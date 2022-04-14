7b26830212acb73bf8d962356546b79d4c994218
prashant <k.prashant093@gmail.com>
2022-04-14 21:47:27 +0530

    Binary search

---
+++ src/arrays/searchAlgo/BinarySearch.java
@@ -0,0 +1,33 @@
+package arrays.searchAlgo;
+
+public class BinarySearch {
+
+    // Only applicable on monotonic functions
+    public static int binarySearch(int[] arr, int size, int key) {
+        int start = 0;
+        int end = size-1;
+        int mid = start + (end-start)/2;
+
+        while(start <= end) {
+            if(arr[mid] == key) {
+                return mid;
+            }
+
+            if(arr[mid] < key) {
+                start = mid + 1;
+            }
+
+            if(arr[mid] > key) {
+                end = mid - 1;
+            }
+
+            mid = start + (end-start)/2;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 6, 8};
+        System.out.println(binarySearch(arr, 4, 8));
+    }
+}
