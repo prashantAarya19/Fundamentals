cf36ef916ffdfb1b0469908756ef843af745bb22
prashant <k.prashant093@gmail.com>
2022-04-18 00:16:33 +0530

    Pivot array bug fix

--- src/arrays/binarySearch/PivotInArray.java
+++ src/arrays/binarySearch/PivotInArray.java
@@ -7,7 +7,7 @@ public class PivotInArray {
         int end = length - 1;
         int mid = start + (end - start)/2;
         while(start < end) {
-            if(arr[mid] >= arr[start]) {
+            if(arr[mid] >= arr[0]) {
                 start = mid + 1;
             } else {
                 end = mid;
@@ -18,7 +18,10 @@ public class PivotInArray {
     }
 
     public static void main(String[] args) {
-        int[] arr = {4, 6, 8, 9, 2};
+       // int[] arr = {4, 6, 8, 9, 2};
+       // int[] arr = {8, 9, 10, 11, 2, 3, 5, 6};
+        //int[] arr = {6, 7, 9, 1, 2, 3};
+        int[] arr = {8, 10, 17, 1, 3};
         System.out.println(pivotInArray(arr, 5));
     }
 }
