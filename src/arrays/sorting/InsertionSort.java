f193e1a7bf84c3b1a5a4c5a0ab6ce1590bce5f2f
prashant <k.prashant093@gmail.com>
2022-04-21 00:52:36 +0530

    Insertion sort

---
+++ src/arrays/sorting/InsertionSort.java
@@ -0,0 +1,47 @@
+package arrays.sorting;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+
+    public static void main(String[] args) {
+        int[] arr = {25, 2, 10, 9, 3, 4, 6, 8, 15};
+        Arrays.stream(arr).forEach(e -> System.out.print(e + " "));
+        System.out.println();
+        insertionSort(arr);
+        Arrays.stream(arr).forEach(e -> System.out.print(e + " "));
+    }
+
+    public static void sortArray(int[] arr) {
+        // we are starting i = 1 as 1 element will always be sorted.
+        // Hint: Arranging a card one by one.
+        for (int i = 1; i < arr.length; i++) {
+            int j = i - 1;
+            int minElm = arr[i];
+            while (j >= 0) {
+                if (minElm < arr[j]) arr[j + 1] = arr[j];
+                else break;
+                --j;
+            }
+            // By the time the flow will come here we would have got actual position of minElm
+            arr[j + 1] = minElm;
+        }
+    }
+
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int minElm = arr[i];
+            int j = i - 1;
+            while (j >= 0) { // This will traverse backwards in the array
+                if (minElm < arr[j])
+                    arr[j + 1] = arr[j];
+                else
+                    break;
+                --j;
+            }
+            arr[j+1] = minElm;
+        }
+    }
+
+
+}
