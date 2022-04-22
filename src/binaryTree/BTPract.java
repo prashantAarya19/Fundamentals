58b190ed375f503858d81fe70797086a4a320c53
prashant <k.prashant093@gmail.com>
2022-04-23 00:27:35 +0530

    Binary Tree with formatting

--- src/binaryTree/BTPract.java
+++ src/binaryTree/BTPract.java
@@ -4,42 +4,44 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-public class BinaryTree {
+public class BTPract {
 
-    public static Node<Integer> buildTree(Node<Integer> root) {
-        System.out.println("Enter the data::");
+    public static Node<Integer> createBinTree(Node<Integer> rootNode) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter data : ");
         int data = sc.nextInt();
-        root = new Node<>(data);
+        rootNode = new Node<>(data);
 
         if (data == -1) {
             return null;
         }
 
-        System.out.println("Enter data for inserting in left of: " + data);
-        root.left = buildTree(root.left);
-        System.out.println("Enter data for inserting in right: " + data);
-        root.right = buildTree(root.right);
-        return root;
+        System.out.println("Insert data at left of : " + data);
+        rootNode.left = createBinTree(rootNode.left);
+        System.out.println("Insert data at right of :" + data);
+        rootNode.right = createBinTree(rootNode.right);
+
+        return rootNode;
     }
 
-    public static void levelOrderTraversal(Node<Integer> root) {
+    public static void levelOrderTraversal(Node<Integer> rootNode) {
         Queue<Node<Integer>> queue = new LinkedList<>();
-        queue.add(root);
-        // for formatting to a tree
+        queue.add(rootNode);
         queue.add(null);
 
         while (!queue.isEmpty()) {
             Node<Integer> temp = queue.remove();
             if (temp == null) {
                 System.out.println();
-                if (!queue.isEmpty())
+                if (!queue.isEmpty()) {
                     queue.add(null);
+                }
             } else {
                 System.out.print(temp.data + " ");
                 if (null != temp.left) {
                     queue.add(temp.left);
                 }
+
                 if (null != temp.right) {
                     queue.add(temp.right);
                 }
@@ -48,7 +50,7 @@ public class BinaryTree {
     }
 
     public static void main(String[] args) {
-        Node<Integer> root = buildTree(null);
-        levelOrderTraversal(root);
+        Node<Integer> binTree = createBinTree(null);
+        levelOrderTraversal(binTree);
     }
 }
