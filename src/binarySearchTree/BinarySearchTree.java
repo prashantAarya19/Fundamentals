e39a0fcae9d961eab92a995f87e937b5ce823791
prashant <k.prashant093@gmail.com>
2022-04-24 01:48:34 +0530

    deletion in binary tree

--- src/binarySearchTree/BinarySearchTree.java
+++ src/binarySearchTree/BinarySearchTree.java
@@ -67,11 +67,67 @@ public class BinarySearchTree {
         inorderTraversal(rootNode.right);
     }
 
+    public static int minValue(Node<Integer> rootNode) {
+        Node<Integer> temp = rootNode;
+
+        while(null != temp.left) {
+            temp = temp.left;
+        }
+        return temp.data;
+    }
+
+    public static int mixValue(Node<Integer> rootNode) {
+        Node<Integer> temp = rootNode;
+
+        while(null != temp.right) {
+            temp = temp.right;
+        }
+        return temp.data;
+    }
+
+    public static Node<Integer> deleteNode(Node<Integer> root, int k) {
+        if(root == null) {
+            return root;
+        }
+        if(root.data == k) {
+            // 0 child
+            if(root.left == null && root.right == null) {
+                root.data = null;
+                return root;
+            }
+            // left child
+            if(null != root.left && null == root.right) {
+                Node<Integer> temp = root.left;
+                root.left = null;
+                return temp;
+            }else if(null == root.left) {
+                Node<Integer> temp = root.right;
+                root.right = null;
+                return temp;
+            } else {
+                int min = minValue(root.right);
+                root.data = min;
+                root.right = deleteNode(root.right, min);
+                return root;
+            }
+        } else if(root.data > k){
+            root.left = deleteNode(root.left, k);
+            return root;
+        } else {
+            root.right = deleteNode(root.right, k);
+            return root;
+        }
+    }
+
     public static void main(String[] args) {
         Node<Integer> binarySearchTree = takeInput(null);
-        System.out.println("***Inorder traversal");
-        inorderTraversal(binarySearchTree);
+//        System.out.println("***Inorder traversal");
+//        inorderTraversal(binarySearchTree);
+        // 1, 3, 5, 6, 4
         System.out.println("\n***Level order traversal");
+        levelOrderTraversal(binarySearchTree);
+        deleteNode(binarySearchTree, 4);
+        System.out.println("\n***after deletion");
         levelOrderTraversal(binarySearchTree);
     }
 
